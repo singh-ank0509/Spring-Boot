340cc48b6a00ba1d26db2416989ce7aeeb635a22
singh-ank0509 <117641692+singh-ank0509@users.noreply.github.com>
2024-06-23 00:06:03 +0530

    api-gateway initial commit

---
+++ microservices-api-gateway/src/main/java/com/ankur/gateway/MicroservicesApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ankur.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroservicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApiGatewayApplication.class, args);
+	}
+
+}
