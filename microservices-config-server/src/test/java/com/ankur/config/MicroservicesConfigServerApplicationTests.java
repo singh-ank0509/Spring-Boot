a13cf9ea0d1fc2a6aa9cc60c3b8a14c847311d53
singh-ank0509 <117641692+singh-ank0509@users.noreply.github.com>
2024-07-04 21:35:41 +0530

    config server initial commit

---
+++ microservices-config-server/src/test/java/com/ankur/config/MicroservicesConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankur.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
